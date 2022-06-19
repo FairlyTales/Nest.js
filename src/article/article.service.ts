3f62aa9256758be9726998693d5494505697ab03
khnychkin_kirill <khnychkin_kirill@tutanota.com>
2022-06-19 17:09:21 +0300

    added create article functionality

--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -1,8 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { UserEntity } from '@app/user/user.entity';
+import { CreateArticleDto } from '@app/article/dto/createArticle.dto';
+import { ArticleEntity } from '@app/article/article.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ArticleService {
-  async createArticle() {
-    return 'Hello World!';
+  constructor(
+    @InjectRepository(ArticleEntity)
+    private readonly articleRepository: Repository<ArticleEntity>,
+  ) {}
+
+  async createArticle(
+    user: UserEntity,
+    createArticleDto: CreateArticleDto,
+  ): Promise<ArticleEntity> {
+    const article = new ArticleEntity();
+    Object.assign(article, createArticleDto);
+
+    if (!article.tagList) {
+      article.tagList = [];
+    }
+
+    //TODO: add slug generation and remove this temporary placeholder
+    article.slug = 'temp';
+
+    // ArticleEntity doesn't have an author property, it's a relation with UserEntity in typeorm
+    article.author = user;
+
+    return await this.articleRepository.save(article);
   }
 }
