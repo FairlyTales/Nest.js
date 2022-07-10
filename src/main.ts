00eccc6ed5f8af78767a9f07b7012b26d8af0bae
khnychkin_kirill <khnychkin_kirill@tutanota.com>
2022-07-10 14:46:19 +0300

    added swagger generation

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,23 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '@app/app.module';
+
 if (process.env.NODE_ENV !== 'development') {
   require('module-alias/register');
 }
 
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from '@app/app.module';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('MediumCloneSwagger')
+    .setDescription('The Medium Clone Api')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3000);
 }
 
